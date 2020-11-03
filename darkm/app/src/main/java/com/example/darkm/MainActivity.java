3d2c134fd1c86e2ad1d80e2951b0cab44ad11199
Raheem Shah <66431658+Raheemshah2809@users.noreply.github.com>
2020-11-03 12:12:04 +0000

    Dark mode Unzipped.
    Unzipped version of Darkmode.zip

---
+++ darkm/app/src/main/java/com/example/darkm/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.darkm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.DarkTheme);
+        setTheme(R.style.LightTheme);
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.DarkTheme);
+        } else {
+            setTheme(R.style.LightTheme);
+        }
+
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        TextView tvChangeTheme = findViewById(R.id.tvChangeTheme);
+        tvChangeTheme.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }
+
+                finish();
+                startActivity(new Intent(MainActivity.this, MainActivity.this.getClass()));
+            }
+
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+}}
+
+
+
+
+
+
